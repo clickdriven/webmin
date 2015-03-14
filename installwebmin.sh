892e0865e89e87d96ed9dfdfd688b8db63ef26aa
clickdriven <admin@clickdriven.com>
2015-03-14 11:46:42 -0400

    Update installwebmin.sh

--- installwebmin.sh
+++ installwebmin.sh
@@ -1,49 +1,41 @@
-clear
-echo 'Adds Several RPG & EPEL Repositories'
+# Adds Several RPG & EPEL Repositories
 wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
 rpm -i rpmforge-release-0.5.3-1.el6.rf.*.rpm
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 
-echo 'Install Group Development Tools'
-yum groupinstall 'Development Tools'
+# Adds Group Development Tools and Other 
+aspell-devel bison byacc bzip2-devel cscope ctags cvs diffstat doxygen flex httpd-devel ImageMagick freetype-devel gcc gcc-c++ gcc-gfortran gettext git gzip gmp-devel indent intltool libcurl-devel libtool libxml2-devel libicu-devel libjpeg-devel libpng-devel libtidy-devel libvpx-devel libXpm-devel libxslt-devel mysql mysql-devel mysql-server openssl-devel patch patchutils perl-GDGraph proftpd readline-devel t1lib-devel perl perl-GDGraph php php-dom php-gd php-mbstring php-mysql php-pdo php-imap php-ldap php-mcrypt rcs redhat-rpm-config rpm-build subversion swig systemtap
 
-echo 'Install Other Core and Useful Software'
-yum install yum install ImageMagick perl-GDGraph libxml2-devel httpd-devel libXpm-devel gmp-devel libicu-devel t1lib-devel aspell-devel openssl-devel bzip2-devel gzip libcurl-devel libjpeg-devel libvpx-devel libpng-devel freetype-devel readline-devel libtidy-devel libxslt-devel mysql mysql-server mysql-devel php php-pdo php-gd php-dom php-mbstring php-mysql php-imap php-ldap php-mcrypt
-
-echo 'Install ProFTP'
-yum install proftpd
-
-echo 'Checking for Updates' 
+# checks for updates
 yum clean all
 yum update
 
-echo 'MySQL Setup'
-yum install mysql-server
+# MySQL Setup
 chkconfig mysqld on
 chkconfig httpd on
 /etc/init.d/httpd start
 /etc/init.d/httpd restart
 service iptables restart
 
-echo 'Install Webmin'
+# Install Webmin
 wget http://prdownloads.sourceforge.net/webadmin/webmin-1.730-1.noarch.rpm
 rpm -U webmin-1.730-1.noarch.rpm
 
-echo 'Install Virtualmin'
+# Install Virtualmin
 wget http://software.virtualmin.com/gpl/scripts/install.sh
 chmod +x install.sh
 ./install.sh
 
-echo 'Install ConfigServer'
-rm -fv csf.tgz
+#Install ConfigServer
 wget http://www.configserver.com/free/csf.tgz
 tar -xzf csf.tgz
 cd csf
 sh install.sh
-yum install perl-GDGraph
+# Install the csf webmin module in:
+# Webmin > Webmin Configuration > Webmin Modules >
+# From local file > /usr/local/csf/csfwebmin.tgz > Install Module
 
-#  Install the csf webmin module in:
-#  Webmin > Webmin Configuration > Webmin Modules >
-#  From local file > /usr/local/csf/csfwebmin.tgz > Install Module
+
+
