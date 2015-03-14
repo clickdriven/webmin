7e9d493f063a7e2c60345b46f90ebe4906732181
clickdriven <admin@clickdriven.com>
2015-03-14 08:55:42 -0400

    Update installwebmin.sh

--- installwebmin.sh
+++ installwebmin.sh
@@ -1,5 +1,50 @@
-
-
-
 clear
 echo 'Adds Several RPG & EPEL Repositories'
+wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
+rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
+rpm -i rpmforge-release-0.5.3-1.el6.rf.*.rpm
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
+
+echo 'Install Group Development Tools'
+yum groupinstall 'Development Tools'
+
+echo 'Install Other Core and Useful Software'
+yum install yum install ImageMagick perl-GDGraph libxml2-devel httpd-devel libXpm-devel gmp-devel libicu-devel t1lib-devel aspell-devel openssl-devel bzip2-devel gzip libcurl-devel libjpeg-devel libvpx-devel libpng-devel freetype-devel readline-devel libtidy-devel libxslt-devel mysql mysql-server mysql-devel php php-pdo php-gd php-dom php-mbstring php-mysql php-imap php-ldap php-mcrypt
+
+echo 'Install ProFTP'
+yum install proftpd
+
+echo 'Checking for Updates' 
+yum clean all
+yum update
+
+echo 'MySQL Setup'
+yum install mysql-server
+chkconfig mysqld on
+chkconfig httpd on
+/etc/init.d/httpd start
+/etc/init.d/httpd restart
+service iptables restart
+
+echo 'Install Webmin'
+wget http://prdownloads.sourceforge.net/webadmin/webmin-1.730-1.noarch.rpm
+rpm -U webmin-1.730-1.noarch.rpm
+
+echo 'Install Virtualmin'
+wget http://software.virtualmin.com/gpl/scripts/install.sh
+chmod +x install.sh
+./install.sh
+
+echo 'Install ConfigServer'
+rm -fv csf.tgz
+wget http://www.configserver.com/free/csf.tgz
+tar -xzf csf.tgz
+cd csf
+sh install.sh
+yum install perl-GDGraph
+
+#  Install the csf webmin module in:
+#  Webmin > Webmin Configuration > Webmin Modules >
+#  From local file > /usr/local/csf/csfwebmin.tgz > Install Module
