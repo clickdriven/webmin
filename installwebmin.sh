4b8672071508b192ee657367746632340c853d3d
clickdriven <admin@clickdriven.com>
2015-03-14 09:12:14 -0400

    Update installwebmin.sh

--- installwebmin.sh
+++ installwebmin.sh
@@ -1,7 +1,7 @@
 clear
 echo 'Adds Several RPG & EPEL Repositories'
 wget http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm
-rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
+#rpm --import http://apt.sw.be/RPM-GPG-KEY.dag.txt
 rpm -i rpmforge-release-0.5.3-1.el6.rf.*.rpm
 wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
