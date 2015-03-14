76f5fa439bd9da80639a3e75f52c0c257b95d43c
clickdriven <admin@clickdriven.com>
2015-03-14 11:51:28 -0400

    Update installwebmin.sh

--- installwebmin.sh
+++ installwebmin.sh
@@ -6,7 +6,7 @@ wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
 
 # Adds Group Development Tools and Other 
-aspell-devel bison byacc bzip2-devel cscope ctags cvs diffstat doxygen flex httpd-devel ImageMagick freetype-devel gcc gcc-c++ gcc-gfortran gettext git gzip gmp-devel indent intltool libcurl-devel libtool libxml2-devel libicu-devel libjpeg-devel libpng-devel libtidy-devel libvpx-devel libXpm-devel libxslt-devel mysql mysql-devel mysql-server openssl-devel patch patchutils perl-GDGraph proftpd readline-devel t1lib-devel perl perl-GDGraph php php-dom php-gd php-mbstring php-mysql php-pdo php-imap php-ldap php-mcrypt rcs redhat-rpm-config rpm-build subversion swig systemtap
+yum install aspell-devel bison byacc bzip2-devel cscope ctags cvs diffstat doxygen flex httpd-devel ImageMagick freetype-devel gcc gcc-c++ gcc-gfortran gettext git gzip gmp-devel indent intltool libcurl-devel libtool libxml2-devel libicu-devel libjpeg-devel libpng-devel libtidy-devel libvpx-devel libXpm-devel libxslt-devel mysql mysql-devel mysql-server openssl-devel patch patchutils perl-GDGraph proftpd readline-devel t1lib-devel perl perl-GDGraph php php-dom php-gd php-mbstring php-mysql php-pdo php-imap php-ldap php-mcrypt rcs redhat-rpm-config rpm-build subversion swig systemtap
 
 # checks for updates
 yum clean all
